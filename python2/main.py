20af85c19819faeb9c67825c681809fdeafb8374
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-05-30 11:56:55 +0900

    Python2 で Structured Property を利用

--- python2/main.py
+++ python2/main.py
@@ -3,9 +3,15 @@ import webapp2
 from google.appengine.ext import ndb
 
 
+class Structured(ndb.Model):
+    kind = ndb.StringProperty()
+    value = ndb.StringProperty()
+
+
 class Book(ndb.Model):
     name = ndb.StringProperty()
-    pass
+    structured = ndb.StructuredProperty(Structured)
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -24,6 +30,7 @@ class CreatePage(webapp2.RequestHandler):
         name = self.request.get('name', default_value='sample book (python27)')
         book = Book(
             name=name,
+            structured=Structured(kind='This is Kind', value='This is value.')
         )
         book.put()
 
