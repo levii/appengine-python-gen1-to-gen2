fd80d7962e00c771d9a8b1e3efe39a08c53b90c5
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-05-30 11:34:49 +0900

    Add sample code of Python2.7

--- python2/main.py
+++ python2/main.py
@@ -1,12 +1,36 @@
 import webapp2
 
+from google.appengine.ext import ndb
+
+
+class Book(ndb.Model):
+    name = ndb.StringProperty()
+    pass
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        query = Book.query()
 
+        results = [book for book in query.fetch()]
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Books:\n\n')
+        self.response.write('\n'.join([str(book) for book in results]))
+
+
+class CreatePage(webapp2.RequestHandler):
+    def get(self):
+        name = self.request.get('name', default_value='sample book (python27)')
+        book = Book(
+            name=name,
+        )
+        book.put()
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(book)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/create', CreatePage)
 ], debug=True)
